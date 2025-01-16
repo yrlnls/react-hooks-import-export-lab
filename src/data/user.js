47dcd96f2d19cbe3b885c4b0a40f235a413c7a1c
tillenotuoma <totuoma27@gmail.com>
2025-01-17 00:44:21 +0300

    updated components files

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,10 @@
 const username = "Liza";
+export { username };
+
 const city = "New York";
+export { city };
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
+export { image };
 
 // export all three variables as named exports
